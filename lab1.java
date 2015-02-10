4b801ad98ecdaeccb4b64e2f92c70d6ebc4904ae
dehari <deharih09@uww.edu>
2015-02-10 16:24:21 -0600

    First commit

---
+++ lab1.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class lab1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		Random generator = new Random();
+		int number = generator.nextInt(100) + 1;
+		System.out.println("Please guess a number between 1 and 
+100");
+		int guess = in.nextInt();
+		if (number == guess){
+			System.out.println("You guessed the correct 
+number: " + guess);
+		}
+		else{
+			System.out.print("Sorry, your guess did not 
+match the number");
+		}
+
+	}
+
+}
